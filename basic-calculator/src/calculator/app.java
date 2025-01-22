a76e746901a3d8e34fac07ba9a2450b8361d80e8
Caroline <carol-html@hotmail.com>
2025-01-22 18:05:26 -0300

    kk

---
+++ basic-calculator/src/calculator/app.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class app {
+
+	public static void main(String[] args) {
+		
+		Calculator myCalculator = new Calculator();
+		
+		int add = myCalculator.add(10, 100);
+		
+		System.out.println(add);
+	}
+}
+
+
