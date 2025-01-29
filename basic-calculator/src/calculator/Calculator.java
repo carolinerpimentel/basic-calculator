ae70e41a5a3887b2121abffee95da1369733bc82
Caroline <carol-html@hotmail.com>
2025-01-29 15:13:12 -0300

    Update
    Some ways in which the program can be improved

--- basic-calculator/src/calculator/Calculator.java
+++ basic-calculator/src/calculator/Calculator.java
@@ -1,30 +1,39 @@
 package calculator;
 
+// This program adds, subtracts, multiplies, divides and displays the remainder between two integers
+
 public class Calculator {
 	
+	int a, b;
+	
 	//Constructor
-		public Calculator(){
-			
+		public Calculator(int a, int b){
+			this.a = a;
+			this.b = b;
 		}
 		
-		public int add(int a, int b) {
+		public int add() {
 			return a + b;
 		}
 		
-		public int subtract(int a, int b) {
+		public int subtract() {
 			return a - b;
 		}
 		
-		public int multiply(int a, int b) {
+		public int multiply() {
 			return a * b; 
 		}
 		
-		public int divide(int a, int b) {
+		public int divide() {
 			return a / b;
 		}
 		
-		public int modulo(int a, int b) {
-			return a % b;
+		public int modulo() {
+			return a % b;	
+		}
+		
+		public String toString() {
+			return a + " + " + b + " = " + add() + "\n" + a + " - " + b + " = " + subtract() + "\n" + a + " * " + b + " = " + multiply() + "\n" + a + " / " + b + " = " + divide() + "\n" + a + " % " + b + " = " + modulo();
 		}
 
 }
--- basic-calculator/src/calculator/app.java
+++ basic-calculator/src/calculator/app.java
@@ -4,11 +4,17 @@ public class app {
 
 	public static void main(String[] args) {
 		
-		Calculator myCalculator = new Calculator();
+		Calculator myCalculator = new Calculator(100, 10);
 		
-		int add = myCalculator.add(10, 100);
-		
-		System.out.println(add);
+		int add = myCalculator.add();
+		int subtract = myCalculator.subtract();
+		int multiply = myCalculator.multiply();
+		int divide = myCalculator.divide();
+		int modulo = myCalculator.modulo();
+				
+		System.out.println(add + "; " + subtract + "; " + multiply + "; " + divide + "; " + modulo);
+		System.out.println("");	
+		System.out.println(myCalculator);
 	}
 }
 
